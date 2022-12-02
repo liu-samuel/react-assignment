47bba5f96406146612902272d403fe60e9d955d6
root <root@DESKTOP-JK8HLM9.localdomain>
2022-12-02 00:18:19 -0500

    embed video

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -15,6 +15,17 @@ const Intro = () => {
             Then, paste the link to the video in the form below.
             (Please make sure that the video is public, otherwise we cannot access it.)
             And try to display it in the region below the form.  */}
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">Rick Roll</a>
+            <iframe
+              width="853"
+              height="480"
+              src={`https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
+
 
         </div>
     )
